e2ce2740ee3ebab219faa0cc15dd50d4e932484b
Timothy Yi <Timothy.Yi@hyland.com>
2020-06-30 15:01:57 -0400

    Implement BoardSpace abstract class

---
+++ BoardSpace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    abstract class BoardSpace
+    {
+        protected string name;
+        public abstract void onPlayerLanding(Player player);
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
