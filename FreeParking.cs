35eae5b7955a5c22b8506ae1b0bdd1bb59d57864
Timothy Yi <Timothy.Yi@hyland.com>
2020-06-30 15:05:02 -0400

    Implement free parking/blank spaces

---
+++ FreeParking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class FreeParking : BoardSpace
+    {
+        public override void onPlayerLanding(Player player)
+        {
+            Console.WriteLine("You landed on free parking!");
+        }
+    }
+}
