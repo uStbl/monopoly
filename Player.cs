02c3e85213fad1760137372090bf260d98b665f9
Timothy Yi <Timothy.Yi@hyland.com>
2020-06-30 16:57:58 -0400

    Add methods for losing and moving

--- Player.cs
+++ Player.cs
@@ -6,22 +6,68 @@ namespace monopoly
 {
     class Player
     {
-        private int id; // Must be between 1 - # of players in the game
-        private int money; // Must be >= 0
-        private int position; // TODO define bounds
+        static private int totalSpaces; // The number of spaces on the game board.
 
-        public int getId() {
+        private int id; // Must be between 1 - # of players in the game.
+        private bool isInGame; // If true, the player is still playing. If false, the player has lost. Cannot be set from false to true.
+        private int money; // Must be >= 0.
+        private int position; // Must be between 0 - (totalSpaces - 1).
+        private int remainingJailTurns;
+
+        public int GetId()
+        {
             return id;
         }
 
-        public int getMoney()
+        public bool HasLost()
+        {
+            return !isInGame;
+        }
+
+        public void LoseGame()
+        {
+            isInGame = false;
+        }
+
+        public int GetMoney()
         {
             return money;
         }
 
-        public void setMoney(int money)
+        public void SetMoney(int money)
         {
             this.money = money;
+        }
+
+        public int GetPosition()
+        {
+            return position;
+        }
+
+        public void Move(int spaces)
+        {
+            position += spaces;
+            position %= totalSpaces;
+        }
+
+        public void MoveTo(int destination)
+        {
+            if (destination < 0 || destination >= totalSpaces)
+            {
+                throw new System.ArgumentOutOfRangeException("destination", "The destination must be between 0 and the number of board spaces - 1.");
+            }
+            else
+                position = destination;
+        }
+
+        public int GetRemainingJailTurns()
+        {
+            return remainingJailTurns;
+        }
+
+        public void SetRemainingJailTurns(int turns)
+        {
+            remainingJailTurns = turns;
         }
     }
 }
