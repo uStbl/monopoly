5113bf21fbcbe2c76f9e0caa3f26c9db4cdd4c8c
Timothy Yi <Timothy.Yi@hyland.com>
2020-06-30 15:01:16 -0400

    Implement player fields

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class Player
+    {
+        private int id; // Must be between 1 - # of players in the game
+        private int money; // Must be >= 0
+        private int position; // TODO define bounds
+
+        public int getId() {
+            return id;
+        }
+
+        public int getMoney()
+        {
+            return money;
+        }
+
+        public void setMoney(int money)
+        {
+            this.money = money;
+        }
+    }
+}
