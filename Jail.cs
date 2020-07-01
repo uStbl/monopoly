74b74b06b9584f212ce8fe7270e4e7b7725d5c77
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-01 15:28:09 -0400

    Add jail space

---
+++ Jail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class Jail : BoardSpace
+    {
+        public override void OnPlayerLanding(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
