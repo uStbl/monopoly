02422592ec037145268de630a52923e5fc33c322
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-01 15:28:00 -0400

    Add Go space

---
+++ GoSpace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class GoSpace : BoardSpace
+    {
+        public const int PassMoney = 200;
+        public override void OnPlayerLanding(Player player)
+        {
+            return;
+        }
+
+        public void OnPlayerPassing(Player player)
+        {
+            player.AddMoney(PassMoney);
+        }
+    }
+}
