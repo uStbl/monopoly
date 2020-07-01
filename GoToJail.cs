da60364fae091aebc975631b761e2beef3f5e12c
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-01 18:13:46 -0400

    Add go to jail space

---
+++ GoToJail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class GoToJail : BoardSpace
+    {
+        private int jailPosition;
+
+        public override void OnPlayerLanding(Player player)
+        {
+            player.MoveTo(jailPosition);
+        }
+    }
+}
