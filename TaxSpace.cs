ab836e733be6450ff4e6fb9be36de0752e8f32e7
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-01 14:19:46 -0400

    Implement tax spaces

---
+++ TaxSpace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class TaxSpace : BoardSpace
+    {
+        private int taxAmount;
+
+        public override void OnPlayerLanding(Player player)
+        {
+            Console.WriteLine("You had to pay {0}.", player.GetMoney());
+            player.AddMoney(-taxAmount);
+        }
+    }
+}
