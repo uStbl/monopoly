4c477a7c1e647b591574a04e6307fbdd0124aa85
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-02 18:01:15 -0400

    Add player prompts

--- Game.cs
+++ Game.cs
@@ -28,7 +28,6 @@ namespace monopoly
                 p.MoveTo(goPosition);
                 p.SetRemainingJailTurns(0);
             }
-            
 
             rnd = new Random();
         }
@@ -45,17 +44,11 @@ namespace monopoly
             {
                 foreach (Player currentPlayer in players)
                 {
+                    Console.WriteLine("It is player {0}'s turn.", currentPlayer.GetId());
                     if (currentPlayer.GetRemainingJailTurns() > 0)
-                    {
-                        JailDiceRoll(currentPlayer);
-                    }
+                        JailTurn(currentPlayer);
                     else
-                    {
-                        DiceRoll(currentPlayer);
-                        BoardSpace landedSpace = boardSpaces[currentPlayer.GetPosition()];
-                        Console.WriteLine("You have landed on {0}.", landedSpace.GetName());
-                        landedSpace.OnPlayerLanding(currentPlayer);
-                    }
+                        NormalTurn(currentPlayer);
 
                     if (currentPlayer.HasLost())
                     {
@@ -70,33 +63,53 @@ namespace monopoly
             }
         }
 
-        private void JailDiceRoll(Player player)
+        private void PromptForEnter()
         {
+            Console.WriteLine("Press enter to roll dice.");
+            ConsoleKey input;
+            do
+            {
+                input = Console.ReadKey(true).Key;
+            } while (input != ConsoleKey.Enter);
+        }
+
+        private void JailTurn(Player player)
+        {
+            Console.WriteLine("You are in jail. If you roll doubles, you will break out of jail and move forward.");
+            PromptForEnter();
             int rollValue1 = rnd.Next(1, 7);
             int rollValue2 = rnd.Next(1, 7);
             if (rollValue1 == rollValue2)
             {
                 player.SetRemainingJailTurns(0);
                 player.Move(rollValue1 + rollValue2);
+                BoardSpace landedSpace = boardSpaces[player.GetPosition()];
+                Console.WriteLine("You rolled {0} and {1} and broke out of jail!", rollValue1, rollValue2);
+                Console.WriteLine("You moved {0} spaces forward and landed on {1}", rollValue1 + rollValue2, landedSpace.GetName());
+                landedSpace.OnPlayerLanding(player);
             }
             else
             {
-                player.SetRemainingJailTurns(player.GetRemainingJailTurns() - 1);
+                int newRemainingTurns = player.GetRemainingJailTurns() - 1;
+                player.SetRemainingJailTurns(newRemainingTurns);
+                Console.WriteLine("Unfortunately, you rolled {0} and {1}.", rollValue1, rollValue2);
+                Console.WriteLine("You must remain in jail for {0} more turns.", newRemainingTurns);
             }
         }
 
         // Roll dice and move [player].
-        // TODO print roll values and prompt player to roll each time
-        private void DiceRoll(Player player)
+        private void NormalTurn(Player player)
         {
             int rollValue1;
             int rollValue2;
             int rollsThisTurn = 0;
             do
             {
+                PromptForEnter();
                 rollValue1 = rnd.Next(1, 7);
                 rollValue2 = rnd.Next(1, 7);
                 rollsThisTurn++;
+                Console.WriteLine("You rolled {0} and {1}.", rollValue1, rollValue2);
 
                 if (rollsThisTurn > MaxDoubleRolls && rollValue1 == rollValue2)
                 {
@@ -108,6 +121,12 @@ namespace monopoly
                 else
                 {
                     player.Move(rollValue1 + rollValue2);
+                    BoardSpace landedSpace = boardSpaces[player.GetPosition()];
+                    Console.WriteLine("You moved {0} spaces forward and landed on {1}", rollValue1 + rollValue2, landedSpace.GetName());
+                    landedSpace.OnPlayerLanding(player);
+                    // TODO break if player loses, do not let them take another turn
+                    if (rollValue1 == rollValue2)
+                        Console.WriteLine("Due to rolling doubles, you get to take another turn!");
                 }
             } while (rollValue1 == rollValue2);
         }
