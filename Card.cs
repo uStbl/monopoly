3ef0fd913026eafb2ff87df7807315b1ad89da17
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-10 17:39:06 -0400

    Add card class

---
+++ Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class Card
+    {
+        private string name;
+        private bool holdable;
+        private Action<Player> effect;
+
+        public Card(string name, bool holdable, Action<Player> effect)
+        {
+            this.name = name;
+            this.holdable = holdable;
+            this.effect = effect;
+        }
+
+        public void DoEffect(Player player)
+        {
+            effect(player);
+        }
+    }
+}
