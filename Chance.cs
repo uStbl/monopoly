88ecc7f660bb8f3bff0947283c3ece851a222112
Timothy Yi <Timothy.Yi@hyland.com>
2020-07-15 17:52:56 -0400

    Separate Chest and Chance into two classes

---
+++ Chance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class Chance : BoardSpace
+    {
+        private Deck deck;
+
+        public Chance(bool isChest)
+        {
+            name = "Chance";
+        }
+
+        public void SetDeck(Deck deck)
+        {
+            this.deck = deck;
+        }
+
+        public override void OnPlayerLanding(Player player)
+        {
+            if (deck.Peek().IsHoldable())
+                player.AddCard(deck.Take());
+            else
+                deck.Cycle().DoEffect(player);
+        }
+    }
+}
---
+++ CommunityChest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monopoly
+{
+    class CommunityChest : BoardSpace
+    {
+        private Deck deck;
+
+        public CommunityChest(bool isChest)
+        {
+            name = "Community Chest";
+        }
+
+        public void SetDeck(Deck deck)
+        {
+            this.deck = deck;
+        }
+
+        public override void OnPlayerLanding(Player player)
+        {
+            if (deck.Peek().IsHoldable())
+                player.AddCard(deck.Take());
+            else
+                deck.Cycle().DoEffect(player);
+        }
+    }
+}
